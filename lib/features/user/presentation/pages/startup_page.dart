68f581e628af0c5a3d28f8714162b084a8a3ded3
charlesarchibong <charlesarchibong10@gmail.com>
2021-05-01 00:07:38 +0100

    refactor codes

--- lib/features/user/presentation/pages/startup_page.dart
+++ lib/features/user/presentation/pages/startup_page.dart
@@ -1,47 +1,58 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:hagglex/core/constants/app_assets.dart';
+import 'package:hagglex/core/navigations/route_paths.dart';
 import 'package:hagglex/core/share_ui/shared/app_colors.dart';
 import 'package:hagglex/core/share_ui/shared/ui_helpers.dart';
+import 'package:hagglex/features/user/presentation/widgets/hagglex_app_backgroun_widget.dart';
 
-class StartupPage extends StatelessWidget {
+class StartupPage extends StatefulWidget {
   const StartupPage({Key key}) : super(key: key);
+
+  @override
+  _StartupPageState createState() => _StartupPageState();
+}
+
+class _StartupPageState extends State<StartupPage> {
+  void _navigateToAnotherPage() {
+    Timer(
+      Duration(seconds: 3),
+      () {
+        Navigator.pushNamed(context, Routes.loginPage);
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _navigateToAnotherPage();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
-      body: SizedBox.expand(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                const Color(0xFF2E1963),
-                const Color(0xFF402B75),
-                const Color(0xFF271160),
-              ],
-              begin: const FractionalOffset(0.0, 0.4),
-              end: const FractionalOffset(1.0, 0.0),
+      body: HaggleXAppBackgroundWidget(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              AppAssets.appLogoImage,
+              width: 50,
+              height: 50,
             ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                AppAssets.appLogoImage,
-                width: 50,
-                height: 50,
+            verticalSpaceSmall,
+            Text(
+              'HaggleX',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 27,
+                fontWeight: FontWeight.w900,
               ),
-              verticalSpaceSmall,
-              Text(
-                'HaggleX',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 27,
-                  fontWeight: FontWeight.w900,
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
