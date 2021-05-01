b279c3ded1434a81e46e4327f8af9d4636c772da
charlesarchibong <charlesarchibong10@gmail.com>
2021-05-01 12:22:28 +0100

    designed login page completely

--- lib/features/user/presentation/pages/login_page.dart
+++ lib/features/user/presentation/pages/login_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hagglex/core/share_ui/shared/app_colors.dart';
+import 'package:hagglex/core/share_ui/shared/input_field.dart';
+import 'package:hagglex/core/share_ui/shared/touachable_opacity.dart';
+import 'package:hagglex/core/share_ui/shared/ui_helpers.dart';
 import 'package:hagglex/features/user/presentation/widgets/hagglex_app_backgroun_widget.dart';
 
 class LoginPage extends StatefulWidget {
@@ -30,7 +33,64 @@ class _LoginPageState extends State<LoginPage> {
                   fontSize: 50,
                   fontWeight: FontWeight.w900,
                 ),
-              )
+              ),
+              verticalSpace(30),
+              InputField(
+                controller: null,
+                label: 'Email Address',
+                password: false,
+              ),
+              verticalSpace(20.5),
+              InputField(
+                controller: null,
+                label: 'Password (Min. 8 characters)',
+                password: true,
+              ),
+              verticalSpace(10.5),
+              Align(
+                alignment: Alignment.topRight,
+                child: Text(
+                  'Forgot Password?',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
+              verticalSpace(44),
+              TouchableOpacity(
+                onTap: () {},
+                child: Container(
+                  width: double.infinity,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Color(0xffFFC175),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'LOG IN',
+                      style: TextStyle(
+                        color: Color(0xff3E0606),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              verticalSpace(33),
+              Center(
+                child: Text(
+                  'New User? Create a new account',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
