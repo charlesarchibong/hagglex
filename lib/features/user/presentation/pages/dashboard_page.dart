9c8c1add3da5c5034770a93575980a032af57f07
charlesarchibong <charlesarchibong10@gmail.com>
2021-05-04 00:54:38 +0100

    logout functionality

--- lib/features/user/presentation/pages/dashboard_page.dart
+++ lib/features/user/presentation/pages/dashboard_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hagglex/core/local_data/user_data/delete_user_session.dart';
+import 'package:hagglex/core/navigations/route_paths.dart';
+import 'package:hagglex/core/share_ui/shared/touachable_opacity.dart';
+import 'package:hagglex/core/share_ui/shared/ui_helpers.dart';
+import 'package:hagglex/injection_container.dart';
 
 class DashboardPage extends StatefulWidget {
   @override
@@ -9,8 +14,29 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Dashboard'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('Dashboard'),
+          verticalSpaceMedium,
+          TouchableOpacity(
+            onTap: () {
+              sl<DeleteUserSession>()();
+              Navigator.pushNamed(
+                context,
+                Routes.loginPage,
+              );
+            },
+            child: Text(
+              'Logout',
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: 30,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
